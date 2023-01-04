c55dbe0b4522c0efe07fca95b50c463bd0f22b2f
Abhinav Kumar Gupta <abhinav.iit19@gmail.com>
2023-01-04 18:28:14 +0530

    1

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import HomeImage from '../../assets/img/background.jpg';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import Page from '../../components/Page';
+import { Grid } from '@material-ui/core';
+
+
+const BackgroundImage = createGlobalStyle`
+  body {
+    background: url(${HomeImage}) repeat !important;
+    background-size: cover !important;
+    background-color: #171923;
+    color: #fff;
+  }
+`;
 
 export default function Dashboard() {
-    return <div>Dashboard</div>;
+    return (
+        <Switch>
+            <Page>
+                <BackgroundImage />
+                <Grid>
+                    
+                </Grid>
+
+            </Page>
+        </Switch>
+    );
 };
