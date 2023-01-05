0c2d51cd8cbfe1803a140c72085277f7b2355486
Abhinav Kumar Gupta <abhinav.iit19@gmail.com>
2023-01-05 16:20:07 +0530

    LatestNews Done

--- src/views/Dashboard/Components/LatestNews.tsx
+++ src/views/Dashboard/Components/LatestNews.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Box, Card, CardContent, Typography, Grid, Button } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    gridItem: {
+        height: '100%',
+        // [theme.breakpoints.up('md')]: {
+        //     height: '90px',
+        // },
+    },
+}));
 
 export default function LatestNews() {
-
+    const classes = useStyles();
     return (
         <>
-            <div>Latest News</div>
+            <Box>
+                <Card className={classes.gridItem} style={{ minHeight: '200px' }}>
+                    <CardContent style={{ textAlign: 'center' }}>
+                        <Typography style={{ color: '#fff', textAlign: 'left', fontSize: 15 }} variant='h5'>Latest News</Typography>
+                    </CardContent>
+                </Card>
+            </Box>
         </>
     )
 }
--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -6,6 +6,7 @@ import Page from '../../components/Page';
 import { Grid } from '@material-ui/core';
 import BombFinanceSummary from './Components/BombFinanceSummary';
 import Bonds from './Components/Bonds';
+import LatestNews from './Components/LatestNews';
 
 
 const BackgroundImage = createGlobalStyle`
@@ -23,11 +24,17 @@ export default function Dashboard() {
       <Page>
         <BackgroundImage />
         <Grid container spacing={3} alignItems="center" style={{ width: "100%" }}>
-          <Grid item spacing={3} style={{ width: "100%" }}>
+          {/* <Grid item spacing={3} style={{ width: "100%" }}>
             <BombFinanceSummary />
           </Grid>
           <Grid item spacing={3} style={{ width: "100%" }}>
             <Bonds />
+          </Grid> */}
+          <Grid item spacing={3} style={{ width: "100%" }}>
+            <LatestNews />
+          </Grid>
+          <Grid item spacing={3} style={{ width: "100%" }}>
+
           </Grid>
         </Grid>
       </Page>
